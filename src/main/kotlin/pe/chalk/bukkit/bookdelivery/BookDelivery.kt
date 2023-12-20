2dd84411bc60867fa02d0679379181ee42758ae0
Chalk <9481405+ChalkPE@users.noreply.github.com>
2023-12-20 20:40:36 +0900

    feat: only apply if book is on hotbar, do not send item that already player is holding

--- src/main/kotlin/pe/chalk/bukkit/bookdelivery/BookDelivery.kt
+++ src/main/kotlin/pe/chalk/bukkit/bookdelivery/BookDelivery.kt
@@ -28,30 +28,46 @@ class BookDelivery: JavaPlugin(), Listener {
         val entity = event.entity
         if (entity !is Player) return
 
-        val book = entity.inventory.find { isWrittenBook(it) } ?: return
-        val locations = getLocationsFromPages(entity, (book.itemMeta as BookMeta).pages)
+        val item = event.item
+        if (checkAlreadyHaveItem(entity, item.itemStack)) return
 
+        val bookMeta = findBookMetaFromPlayer(entity) ?: return
+        val locations = getLocationsFromPages(entity, bookMeta.pages)
+
+        if (locations.isEmpty()) return
         for (location in locations) {
-            if (moveToChest(location, event.item.itemStack)) {
-                event.item.remove()
+            if (moveToChest(location, item.itemStack)) {
+                item.remove()
                 event.isCancelled = true
                 entity.playSound(entity.location, Sound.ENTITY_ARROW_HIT_PLAYER, 0.2f, 0.5f)
-                break
+                return
             }
         }
 
-        if (!event.isCancelled) {
-            entity.playSound(entity.location, Sound.BLOCK_REDSTONE_TORCH_BURNOUT, 0.2f, 0.5f)
-        }
+        entity.playSound(entity.location, Sound.BLOCK_REDSTONE_TORCH_BURNOUT, 0.2f, 0.5f)
+    }
+
+    private fun canBeAdded(a: ItemStack?, b: ItemStack): Boolean {
+        return a != null && a.isSimilar(b) && (a.amount + b.amount) <= a.maxStackSize
+    }
+
+    private fun checkAlreadyHaveItem(player: Player, itemStack: ItemStack): Boolean {
+        return player.inventory.any { canBeAdded(it, itemStack) }
+    }
+
+    private fun findBookMetaFromPlayer(player: Player): BookMeta? {
+        val hotbar = player.inventory.take(9)
+        val book = hotbar.find { isWrittenBook(it) } ?: return null
+
+        val meta = book.itemMeta ?: return null
+        return if (meta is BookMeta) meta else null
     }
 
     private fun isWrittenBook(stack: ItemStack?): Boolean {
         if (stack == null || !books.contains(stack.type)) return false
 
         val itemMeta = stack.itemMeta
-        if (itemMeta !is BookMeta) return false
-
-        return itemMeta.hasPages()
+        return itemMeta is BookMeta && itemMeta.hasPages()
     }
 
     private fun getLocationsFromPages(player: Player, pages: List<String>): List<Location> {
@@ -66,35 +82,24 @@ class BookDelivery: JavaPlugin(), Listener {
         val block = location.world?.getBlockAt(location) ?: return false
 
         val state = block.state
-        if (state !is Container) return false
-
-        return addItemToInventory(state.inventory, itemStack)
-    }
-
-    private fun canBeAdded(a: ItemStack, b: ItemStack): Boolean {
-        return a.isSimilar(b) && (a.amount + b.amount) <= a.maxStackSize
+        return state is Container && addItemToInventory(state.inventory, itemStack)
     }
 
     private fun addItemToInventory(inventory: Inventory, itemStack: ItemStack): Boolean {
         val contents = inventory.contents.filterNotNull()
         val slot = contents.find { canBeAdded(it, itemStack) }
 
-        if (contents.size == inventory.size) {
-            if (slot == null) {
-                return false
-            } else {
-                slot.amount += itemStack.amount
-                inventory.contents = contents.toTypedArray()
-                return true
-            }
-        } else {
-            if (slot == null) {
-                inventory.contents = (contents + itemStack).toTypedArray()
-            } else {
-                slot.amount += itemStack.amount
-                inventory.contents = contents.toTypedArray()
-            }
+        if (slot != null) {
+            slot.amount += itemStack.amount
+            inventory.contents = contents.toTypedArray()
             return true
         }
+
+        if (contents.size < inventory.size) {
+            inventory.contents = (contents + itemStack).toTypedArray()
+            return true
+        }
+
+        return false
     }
 }
